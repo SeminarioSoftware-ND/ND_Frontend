c6ce933200805069e234ff00168b98fae57c54ea
Sonia Villeda <sonia08_m@hotmail.com>
2019-12-16 09:04:44 -0600

    Cambio AXIOS

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { timeout } from "q";
 
 const instance = axios.create({
-  baseURL: "https://34ba82e7.ngrok.io",
+  baseURL: "https://47f70627.ngrok.io",
   timeout: 70000
 });
 
